3e7e57c627788d86b867287bd99a505a1702fecb
djy <djybeyond@163.com>
2026-03-19 22:18:59 +0800

    optimize

--- lib/presentation/bloc/home/home_bloc.dart
+++ lib/presentation/bloc/home/home_bloc.dart
@@ -1,31 +1,31 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop/domain/entities/banner_data.dart';
-import 'package:shop/domain/entities/category_data.dart';
 import 'package:shop/domain/entities/data_result.dart';
-import 'package:shop/domain/entities/product_data.dart';
 import 'package:shop/domain/usecase/home_usecase.dart';
 import 'package:shop/presentation/bloc/home/home_event.dart';
 import 'package:shop/presentation/bloc/home/home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  List<BannerData> _listBanner = List.empty();
-  List<CategoryData> _listCategory = List.empty();
-  List<ProductData> _listProduct = List.empty();
-
   final HomeUsecase _homeUsecase;
   HomeBloc(this._homeUsecase) : super(HomeState.initial()) {
-    on<HomeEvent>((event, emit) {
-      _fetchBanners(emit);
-      _fetchCategory(emit);
-      _fetchProduct(emit);
-    });
+    on<HomeEvent>(_onEvent);
   }
+
+  Future<void> _onEvent(HomeEvent event, Emitter<HomeState> emit) async {
+    await event.map(
+      loading: (_) async {
+        emit(HomeState.initial());
+        await _fetchBanners(emit);
+        await _fetchCategory(emit);
+        await _fetchProduct(emit);
+      },
+    );
+  }
+
   Future<void> _fetchBanners(Emitter<HomeState> emit) async {
     final result = await _homeUsecase.getBanner();
     switch (result) {
       case Success(data: final bannerList):
-        _listBanner = [..._listBanner, ...bannerList];
-        emit(state.copyWith(bannerList: _listBanner, isLoadingBanner: false));
+        emit(state.copyWith(bannerList: bannerList, isLoadingBanner: false));
         break;
       case Failure(message: final msg):
         emit(state.copyWith(isLoadingBanner: false, errorMessage: msg));
@@ -37,9 +37,8 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     final result = await _homeUsecase.getCategory();
     switch (result) {
       case Success(data: final categoryList):
-        _listCategory = [..._listCategory, ...categoryList];
         emit(
-          state.copyWith(isLoadingCategory: false, categoryList: _listCategory),
+          state.copyWith(isLoadingCategory: false, categoryList: categoryList),
         );
         break;
       case Failure(message: final msg):
@@ -52,10 +51,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     final result = await _homeUsecase.getProduct();
     switch (result) {
       case Success(data: final list):
-        _listProduct = [..._listProduct, ...list];
-        emit(
-          state.copyWith(productList: _listProduct, isLoadingProduct: false),
-        );
+        emit(state.copyWith(productList: list, isLoadingProduct: false));
         break;
       case Failure(message: final msg):
         emit(state.copyWith(errorMessage: msg, isLoadingProduct: false));
